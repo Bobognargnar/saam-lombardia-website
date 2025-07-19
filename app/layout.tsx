673c3ebcb82101ea4d7a1c41f247368bccfdc4ac
Fabrizio La Rosa <fabrizio.larosa@hotiday.it>
2025-07-19 14:32:59 +0200

    fix: moving meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,9 +9,6 @@ export const metadata: Metadata = {
   title: "Sala D'Arme Achille Marozzo Lombardia - Arti Marziali Storiche Italiane",
   description: "Impara le basi della scherma storica in Lombardia - Uno sport di onore e amicizia",
   generator: 'v0.dev',
-  verification: {
-    google: '8I6jF-si9ixcT55N5Krdk150qSORCTXUW0rwxTS270c',
-  },
 }
 
 export default function RootLayout({
@@ -21,6 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="it">
+      <head>
+        <meta name="google-site-verification" content="8I6jF-si9ixcT55N5Krdk150qSORCTXUW0rwxTS270c" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
